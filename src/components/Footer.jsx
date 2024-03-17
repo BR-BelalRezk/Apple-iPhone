82705597f386485f7a8993d556d298606f3f9d0c
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-03-17 15:27:43 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,15 @@
-import React from 'react'
-import { footerLinks } from '../constants'
+import { footerLinks } from "../constants/constants";
 
-export default function Footer () {
+export default function Footer() {
   return (
     <footer className="py-5 sm:px-10 px-5">
       <div className="mx-auto relative max-w-[1120px]">
         <div>
           <p className="font-semibold text-gray text-xs">
-            More ways to shop: {' '}
-            <span className="underline text-blue">
-            Find an Apple Store {' '}
-            </span>
-            or {' '}
-            <span className="underline text-blue">
-            other retailer
-            </span>{' '}
-            near you.
+            More ways to shop:{" "}
+            <span className="underline text-blue">Find an Apple Store </span>
+            or <span className="underline text-blue">other retailer</span> near
+            you.
           </p>
           <p className="font-semibold text-gray text-xs">
             Or call 000800-040-1966
@@ -25,11 +19,13 @@ export default function Footer () {
         <div className="bg-neutral-700 my-5 h-[1px] w-full" />
 
         <div className="flex md:flex-row flex-col md:items-center justify-between">
-          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
+          <p className="font-semibold text-gray text-xs">
+            Copright @ 2024 Apple Inc. All rights reserved.
+          </p>
           <div className="flex">
             {footerLinks.map((link, i) => (
               <p key={link} className="font-semibold text-gray text-xs">
-                {link}{' '}
+                {link}{" "}
                 {i !== footerLinks.length - 1 && (
                   <span className="mx-2"> | </span>
                 )}
@@ -39,5 +35,5 @@ export default function Footer () {
         </div>
       </div>
     </footer>
-  )
+  );
 }
